0fbda853eecf8c2020bcdf44651ff684a27e06de
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2021-01-30 14:24:28 +0100

    5th commit

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-const labels = document.querySelectorAll('.form-control label')
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-    .split('')
-    .map((letter,idx) => `<span style="transition-delay:${idx * 30}ms">${letter}</span>`)
-    .join('')
-})
-=======
-const labels = document.querySelectorAll('.form-control label')
 
+const labels = document.querySelectorAll('.form-control label')
 labels.forEach(label => {
     label.innerHTML = label.innerText
     .split('')
     .map((letter,idx) => `<span style="transition-delay:${idx * 30}ms">${letter}</span>`)
     .join('')
 })
->>>>>>> 4a086f82f2b6593884827f86c82d799d99c0e941
